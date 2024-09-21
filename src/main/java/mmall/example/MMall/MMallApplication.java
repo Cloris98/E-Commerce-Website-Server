71e52799c0ee6b7296f83bfb8b661fb15837f927
Jingwei He <jingweihe@Jingweis-MacBook-Air.local>
2024-09-20 17:34:00 -0700

    first commit init project

---
+++ src/main/java/mmall/example/MMall/MMallApplication.java
@@ -0,0 +1,13 @@
+package mmall.example.MMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mmall/example/MMall/MMallApplicationTests.java
@@ -0,0 +1,13 @@
+package mmall.example.MMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
